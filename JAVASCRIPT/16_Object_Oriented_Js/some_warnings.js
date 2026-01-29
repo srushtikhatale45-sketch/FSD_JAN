0e6eb14c6cfae7cb0b1c5fbfd8ae436314ddfc2a
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-01-29 18:49:19 +0530

    My First Project

--- JAVASCRIPT/16_Object_Oriented_Js/some_warnings.js
+++ JAVASCRIPT/16_Object_Oriented_Js/some_warnings.js
@@ -10,7 +10,31 @@ const user1={
 user1.about();
 
 // user1.about();//Srushti 21
-// Here this of about function to myfunc
+// Here this of about is pointing to object instance of user1
+const myfunc = user1.about;
+myfunc();
+// we assigned about function to myfunc
 /*
-What is actually happening is:
-const
+     what is actually happening is :
+     const myfunc = function(){
+
+            console.log(this);
+
+            console.log(this.first_name,this.age);
+    }
+   
+    So its in global scope and here this pointing to global window object
+    hence when we call myfunc();
+    it gives undefined undefined
+    Because there os no other object instance for reference to this keyword
+
+*/
+
+// to Solve this problem we need to bind the function with object
+// so lets use bind
+
+
+const myfunc1 = user1.about.bind(user1);
+myfunc1();
+
+// Problem solved :
