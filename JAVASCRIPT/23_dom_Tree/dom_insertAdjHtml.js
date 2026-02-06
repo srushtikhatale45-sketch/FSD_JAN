ba94757b917f9ba7152178e4945ec16457050303
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-02-06 16:08:13 +0530

    DOM Tree

---
+++ JAVASCRIPT/23_dom_Tree/dom_insertAdjHtml.js
@@ -0,0 +1,19 @@
+// elem.insertAdjacentHTML(where,html) --> Used Less/Method
+
+// beforebegin
+// afterbegin
+// beforend
+// // afterend
+
+const todolist = document.querySelector(".todo-list");
+// todolist.insertAdjacentHTML("beforeend","<li>beforeEnd</li>");
+// console.log(todolist);
+
+// const todolist = document.querySelector(".todo-list");
+// todolist.insertAdjacentHTML("afterbegin","<li>afterBegin</li>");
+// console.log(todolist);
+// const todolist = document.querySelector(".todo-list");
+// todolist.insertAdjacentHTML("beforebegin","<li>beforebegin</li>");
+
+// const todolist = document.querySelector(".todo-list");
+// todolist.insertAdjacentHTML("afterend","<li>afterend</li>");
